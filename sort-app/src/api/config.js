0e2d9932c9612bd0decb179e03c2071e3c57943f
orlyone <orlando@upcompany.cl>
2021-11-04 17:28:01 -0300

    a

--- sort-app/src/api/config.js
+++ sort-app/src/api/config.js
@@ -1,3 +1,3 @@
-// export const basePath = "https://puc.upwebinar.cl/api"
-export const basePath = "http://localhost:8080/api";
+export const basePath = "https://fantrap.upwebinar.cl/api"
+//export const basePath = "http://localhost:8080/api";
 export const apiVersion = "v1";
