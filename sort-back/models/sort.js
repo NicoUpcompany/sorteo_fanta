2b7e73eaaa8d6cf809c16337066e0b803a2e5bd5
Nicolás <nicolas@upcompany.cl>
2021-11-08 21:29:05 -0300

    back

--- sort-back/models/sort.js
+++ sort-back/models/sort.js
@@ -6,7 +6,8 @@ const sortSchema = Schema({
     winner: {type: String, default:''},
     requirements:{type: Boolean, default: false},
     phone: {type:Number, default:0},
-    time_sort: {type:String, default:0}
+    time_sort: {type:String, default:0},
+    premio: {type:String}
 });
 
 module.exports = mongoose.model("Sort", sortSchema);
--- sort-back/models/sortReg.js
+++ sort-back/models/sortReg.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema;
 const sortRegSchema = Schema({
 	n_sort : {type:Number, required:true}, 
     winner: {type: String, default:''},
-    requirements:{type: Boolean, default: false},
+    requirements:{type: Boolean, default: true},
     phone: {type:Number, default:0},
-    time_sort: {type:String, default:0}
+    time_sort: {type:String, default:0},
+    premio: {type:String}
 });
 
 module.exports = mongoose.model("SortRegion", sortRegSchema);
